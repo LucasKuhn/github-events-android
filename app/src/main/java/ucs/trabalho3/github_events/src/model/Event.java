1ee690876f1dd4cc6c70ad9a7967b3616cc6e48c
Lucas Kuhn <lucas@nordweg.com>
2020-06-26 22:50:32 -0300

    adds code to run background service every 5 seconds, shows event created_at as X days ago

--- app/src/main/java/ucs/trabalho3/github_events/src/model/Event.java
+++ app/src/main/java/ucs/trabalho3/github_events/src/model/Event.java
@@ -1,8 +1,15 @@
 package ucs.trabalho3.github_events.src.model;
 
+import android.os.Build;
+import android.text.format.DateUtils;
+
+import androidx.annotation.RequiresApi;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.time.Instant;
+import java.util.Date;
 import java.util.List;
 
 public class Event {
@@ -19,6 +26,9 @@ public class Event {
     @SerializedName("payload")
     @Expose
     private Payload payload;
+    @SerializedName("created_at")
+    @Expose
+    private String created_at;
 
     public String getId() {
         return id;
@@ -50,6 +60,20 @@ public class Event {
 
     public void setPayload(Payload payload) {
         this.payload = payload;
+    }
+
+    public String getCreatedAt() { return created_at; }
+
+    // https://stackoverflow.com/questions/2201925/converting-iso-8601-compliant-string-to-java-util-date
+    // https://stackoverflow.com/questions/3859288/how-to-calculate-time-ago-in-java
+    // https://stackoverflow.com/questions/35858608/how-to-convert-time-to-time-ago-in-android
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public String getTimeAgo() {
+        Date date = Date.from(Instant.parse(created_at));
+        long time = date.getTime();
+        long now = System.currentTimeMillis();
+        CharSequence ago = DateUtils.getRelativeTimeSpanString(time, now, DateUtils.MINUTE_IN_MILLIS);
+        return ago.toString();
     }
 
 }
